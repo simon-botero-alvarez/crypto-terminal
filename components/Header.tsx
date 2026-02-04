8927028842a2aea45331298ef5f4fe826b3a9d2e
Simon Botero <simonboteroa@gmail.com>
2026-02-03 21:57:50 -0500

    Fix logo src route

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
 		<header>
 			<div className='main-container inner'>
 				<Link href="/">
-					<Image src="logo.svg" alt="Coinpulse Logo" width={132} height={40}></Image>
+					<Image src="/logo.svg" alt="Coinpulse Logo" width={132} height={40}></Image>
 				</Link>
 
 				<nav>
