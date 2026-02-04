52d0f917074fd4c369d856fcf8b27e531e70cac7
Simon Botero <simonboteroa@gmail.com>
2026-02-04 03:58:20 -0500

    Fix Errors on Trending Coins Component

--- components/home/TrendingCoins.tsx
+++ components/home/TrendingCoins.tsx
@@ -1,13 +1,20 @@
-import { fetcher } from '@/lib/coingecko.actions'
+import { fetcher } from '@/lib/coingecko.actions';
+import Link from 'next/link';
+import Image from 'next/image';
 import { cn, formatCurrency, formatPercentage } from '@/lib/utils';
 import { TrendingDown, TrendingUp } from 'lucide-react';
-import Image from 'next/image';
-import Link from 'next/link';
-import DataTable from '../DataTable';
+import DataTable from '@/components/DataTable';
+import { TrendingCoinsFallback } from './fallback';
 
 const TrendingCoins = async () => {
+	let trendingCoins;
 
-	const trendingCoins = await fetcher<{ coins: TrendingCoin[] }>('/search/trending', undefined, 300);
+	try {
+		trendingCoins = await fetcher<{ coins: TrendingCoin[] }>('/search/trending', undefined, 300);
+	} catch (error) {
+		console.error('Error fetching trending coins:', error);
+		return <TrendingCoinsFallback />;
+	}
 
 	const columns: DataTableColumn<TrendingCoin>[] = [
 		{
@@ -21,8 +28,8 @@ const TrendingCoins = async () => {
 						<Image src={item.large} alt={item.name} width={36} height={36} />
 						<p>{item.name}</p>
 					</Link>
-				)
-			}
+				);
+			},
 		},
 		{
 			header: '24h Change',
@@ -33,42 +40,39 @@ const TrendingCoins = async () => {
 
 				return (
 					<div className={cn('price-change', isTrendingUp ? 'text-green-500' : 'text-red-500')}>
-						<p className="flex flex-col items-center">
+						<p className="flex items-center">
+							{formatPercentage(item.data.price_change_percentage_24h.usd)}
 							{isTrendingUp ? (
 								<TrendingUp width={16} height={16} />
 							) : (
 								<TrendingDown width={16} height={16} />
 							)}
-							{formatPercentage(item.data.price_change_percentage_24h.usd)}
 						</p>
 					</div>
-				)
-			}
+				);
+			},
 		},
 		{
 			header: 'Price',
 			cellClassName: 'price-cell',
-			cell: (coin) => formatCurrency(coin.item.data.price)
-		}
-	]
+			cell: (coin) => formatCurrency(coin.item.data.price),
+		},
+	];
 
 	return (
 		<div id="trending-coins">
-
 			<h4>Trending Coins</h4>
 
-			<div id="trending-coins">
-				<DataTable
-					data={trendingCoins.coins.slice(0, 6) || []}
-					columns={columns}
-					rowKey={(coin) => coin.item.id}
-					tableClassName="trending-coin-table"
-					headerCellClassName="py-3!"
-					bodyCellClassName="py-2!"
-				/>
-			</div>
+			<DataTable
+				data={trendingCoins.coins.slice(0, 6) || []}
+				columns={columns}
+				rowKey={(coin) => coin.item.id}
+				tableClassName="trending-coins-table"
+				headerCellClassName="py-3!"
+				bodyCellClassName="py-2!"
+			/>
 		</div>
-	)
-}
+	);
+};
 
-export default TrendingCoins
+export default TrendingCoins;
